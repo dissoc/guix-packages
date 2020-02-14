d7516b6a5ef26cf698adbf45b1413133e1fad812
atticMaverick <atticmaverick@gmail.com>
2020-02-13 18:08:33 -0600

    made channel package again

--- clojure/leiningen.scm
+++ clojure/leiningen.scm
@@ -27,61 +27,65 @@
   #:use-module (gnu packages java))
 
 (define lein-pkg
-   (origin
-     (method url-fetch)
-     (uri "https://raw.github.com/technomancy/leiningen/2.5.1/bin/lein-pkg")
-     (sha256
-      (base32
-       "0pqqb2bh0a17426diwyhk5vbxcfz45rppbxmjydsmai94jm3cgix"))))
+  (origin
+   (method url-fetch)
+   (uri "https://raw.github.com/technomancy/leiningen/2.9.1/bin/lein-pkg")
+   (sha256
+    (base32
+     "1h0gpzpr7xk6hvmrrq41bcp2k9aai348baf8ad9bxvci01n4zb12"))))
 
 (define builder.sh
-   (origin
-     (method url-fetch)
-     (uri "https://raw.githubusercontent.com/NixOS/nixpkgs/0761f81da71fc6a940c7f51129b6c7717db78e87/pkgs/development/tools/build-managers/leiningen/builder.sh")
-     (sha256
-      (base32
-       "10qsz16pnhccwdl34w043kl3c3prkpi1cv4rpzfjfmcqa8kjcny8"))))
-
+  (origin
+   (method url-fetch)
+   (uri "https://raw.githubusercontent.com/NixOS/nixpkgs/0761f81da71fc6a940c7f51129b6c7717db78e87/pkgs/development/tools/build-managers/leiningen/builder.sh")
+   (sha256
+    (base32
+     "10qsz16pnhccwdl34w043kl3c3prkpi1cv4rpzfjfmcqa8kjcny8"))))
 
 (define-public leiningen
   (package
-    (name "leiningen")
-    (version "2.5.1")
-    (source (origin
-               (method url-fetch)
-               (uri (string-append
-                     "https://github.com/technomancy/leiningen/releases/download/"
-                     version "/leiningen-" version "-standalone.zip"))
-               (sha256
-                (base32
-                 "1irl3w66xq1xbbs4g10dnw1vknfw8al70nhr744gfn2za27w0xdl"))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:tests? #f ; no "check" target
-       #:phases
-       (modify-phases %standard-phases
+   (name "leiningen")
+   (version "2.9.1")
+   (source (origin
+            (method url-fetch)
+            ;; supposedly the .zip is actually a .jar but github
+            ;; doesnt allow jar releases or something?
+            ;; i saw that mentioned somewhere but dont recall the source
+            ;; if you java -jar it seems to work so maybe it's true?
+            ;; let me know if you know
+            (uri (string-append
+                  "https://github.com/technomancy/leiningen/releases/download/"
+                  version "/leiningen-" version "-standalone.zip"))
+            (sha256
+             (base32
+              "1y2mva5s2w2szzn1b9rhz0dvkffls4ravii677ybcf2w9wd86z7a"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:tests? #f ; no "check" target
+      #:phases
+      (modify-phases %standard-phases
 
-        (replace 'build
-                 (lambda _
-           (setenv "JAVA_HOME"
-                   (assoc-ref %build-inputs "icedtea7"))
-           ;; Disable tests to avoid dependency on hamcrest-core, which needs
-           ;; Ant to build.  This is necessary in addition to disabling the
-           ;; "check" phase, because the dependency on "test-jar" would always
-           ;; result in the tests to be run.
-           (zero? (system* "bash" "builder.sh"
-                           (string-append "-Ddist.dir="
-                                          (assoc-ref %outputs "out"))))))
-        (delete 'configure)
-        (delete 'unpack)
-        (delete 'install))))
-    (native-inputs
-     `(("builder.sh" ,builder.sh)
-       ("lein-pkg" ,lein-pkg)))
-    (inputs
-     `(("icedtea" ,icedtea "jdk")))
-    (home-page "http://leiningen.org/")
-    (synopsis "Project automation for Clojure")
-    (description
-     "")
-    (license license:epl1.0)))
+                     (replace 'build
+                              (lambda _
+                                (setenv "JAVA_HOME"
+                                        (assoc-ref %build-inputs "icedtea7"))
+                                ;; Disable tests to avoid dependency on hamcrest-core, which needs
+                                ;; Ant to build.  This is necessary in addition to disabling the
+                                ;; "check" phase, because the dependency on "test-jar" would always
+                                ;; result in the tests to be run.
+                                (system* "bash" "builder.sh"
+                                         (string-append "-Ddist.dir="
+                                                        (assoc-ref %outputs "out")))))
+                     (delete 'configure)
+                     (delete 'unpack)
+                     (delete 'install))))
+   (native-inputs
+    `(("builder.sh" ,builder.sh)
+      ("lein-pkg" ,lein-pkg)))
+   (inputs
+    `(("icedtea" ,icedtea "jdk")))
+   (home-page "http://leiningen.org/")
+   (synopsis "Project automation for Clojure")
+   (description
+    "")
+   (license license:epl1.0)))
