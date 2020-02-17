c573875898e144c15182a564211a8783f7325591
atticMaverick <atticmaverick@gmail.com>
2020-02-17 11:50:39 -0600

    oops

--- clojure/leiningen.scm
+++ clojure/leiningen.scm
@@ -4,10 +4,10 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix utils)
+  #:use-module (guix build-system trivial)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
   #:use-module (gnu packages java))
-
 
 
 ;; (define lein-pkg
@@ -94,77 +94,69 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (nongnu packages clojure)
-  #:use-module (guix packages)
-  #:use-module (guix build-system gnu)
-  #:use-module (guix build-system trivial)
-  #:use-module (guix download)
-  #:use-module (guix git-download)
-  #:use-module ((guix licenses) #:prefix license:))
-
 ;; This is a hidden package, as it does not really serve a purpose on its own.
 (define leiningen-jar
   (package
-    (name "leiningen-jar")
-    (version "2.9.1")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/technomancy/leiningen/releases/download/"
-                                  version "/leiningen-2.9.1-standalone.zip"))
-              (file-name "leiningen-standalone.jar")
-              (sha256
-               (base32
-                "1y2mva5s2w2szzn1b9rhz0dvkffls4ravii677ybcf2w9wd86z7a"))))
-    (build-system trivial-build-system)
-    (arguments
-     `(#:modules ((guix build utils))
-       #:builder (begin
-                   (use-modules (guix build utils))
-                   (let ((source (assoc-ref %build-inputs "source"))
-                         (jar-dir (string-append %output "/share/")))
-                     (mkdir-p jar-dir)
-                     (copy-file source
-                                (string-append jar-dir "leiningen-standalone.jar"))))))
-    (home-page "https://leiningen.org")
-    (synopsis "Automate Clojure projects without setting your hair on fire")
-    (description "Leiningen is a Clojure tool with a focus on project
+   (name "leiningen-jar")
+   (version "2.9.1")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://github.com/technomancy/leiningen/releases/download/"
+                                version "/leiningen-2.9.1-standalone.zip"))
+            (file-name "leiningen-standalone.jar")
+            (sha256
+             (base32
+              "1y2mva5s2w2szzn1b9rhz0dvkffls4ravii677ybcf2w9wd86z7a"))))
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils))
+      #:builder (begin
+                  (use-modules (guix build utils))
+                  (let ((source (assoc-ref %build-inputs "source"))
+                        (jar-dir (string-append %output "/share/")))
+                    (mkdir-p jar-dir)
+                    (copy-file source
+                               (string-append jar-dir "leiningen-standalone.jar"))))))
+   (home-page "https://leiningen.org")
+   (synopsis "Automate Clojure projects without setting your hair on fire")
+   (description "Leiningen is a Clojure tool with a focus on project
 automation and declarative configuration.  It gets out of your way and
 lets you focus on your code.")
-    (license license:epl1.0)))
+   (license license:epl1.0)))
 
 (define-public leiningen
   (package
-    (inherit leiningen-jar)
-    (name "leiningen")
-    (version "2.9.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/technomancy/leiningen.git")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0qv9vp6ypdilwv818fpwknr9sj40sz2vdcqxbd42m1l0ljjggiy1"))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:tests? #f
-       #:phases (modify-phases %standard-phases
-                  (delete 'configure)
-                  (delete 'build)
-                  (replace 'install
-                    (lambda _
-                      (let* ((lein-pkg (string-append (assoc-ref %build-inputs "source") "/bin/lein-pkg"))
-                             (lein-jar (string-append (assoc-ref  %build-inputs "leiningen-jar")
-                                                      "/share/leiningen-standalone.jar"))
-                             (bin-dir (string-append %output "/bin"))
-                             (lein (string-append bin-dir "/lein")))
-                        (mkdir-p bin-dir)
-                        (copy-file lein-pkg lein)
-                        (patch-shebang lein)
-                        (chmod lein #o555)
-                        (substitute* lein
-                          (("LEIN_JAR=.*") (string-append "LEIN_JAR=" lein-jar)))
-                        #t))))))
-    (inputs
-     `(("leiningen-jar" ,leiningen-jar)))))
+   (inherit leiningen-jar)
+   (name "leiningen")
+   (version "2.9.1")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/technomancy/leiningen.git")
+                  (commit version)))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0qv9vp6ypdilwv818fpwknr9sj40sz2vdcqxbd42m1l0ljjggiy1"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:tests? #f
+      #:phases (modify-phases %standard-phases
+                              (delete 'configure)
+                              (delete 'build)
+                              (replace 'install
+                                       (lambda _
+                                         (let* ((lein-pkg (string-append (assoc-ref %build-inputs "source") "/bin/lein-pkg"))
+                                                (lein-jar (string-append (assoc-ref  %build-inputs "leiningen-jar")
+                                                                         "/share/leiningen-standalone.jar"))
+                                                (bin-dir (string-append %output "/bin"))
+                                                (lein (string-append bin-dir "/lein")))
+                                           (mkdir-p bin-dir)
+                                           (copy-file lein-pkg lein)
+                                           (patch-shebang lein)
+                                           (chmod lein #o555)
+                                           (substitute* lein
+                                                        (("LEIN_JAR=.*") (string-append "LEIN_JAR=" lein-jar)))
+                                           #t))))))
+   (inputs
+    `(("leiningen-jar" ,leiningen-jar)))))
