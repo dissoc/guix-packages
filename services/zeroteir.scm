27f5a4cc8c74eaea1bed583214d8bcf7bc1ecfa1
atticMaverick <atticmaverick@gmail.com>
2020-04-26 02:04:12 -0500

    hmmm8

--- services/zeroteir.scm
+++ services/zeroteir.scm
@@ -12,13 +12,17 @@
 (define (zerotier-shepherd-service)
   (lambda ()
     (let* ((log-file "/var/log/zerotier.log")
-           (zerotier-package (package zerotier-one)))
+           ;;(zerotier-package (package zerotier-one))
+           )
       (list (shepherd-service
              (documentation "Run zerotier service")
              (provision '(zerotier-one))
              (requirement '(networking))
              (start #~(make-forkexec-constructor
-                       (list (string-append #$zerotier-package "/sbin/zerotier-one"))))
+                       (list (string-append
+                              zerotier-one
+                              ;;#$zerotier-package
+                              "/sbin/zerotier-one"))))
              (stop #~(make-kill-destructor)))))))
 
 
